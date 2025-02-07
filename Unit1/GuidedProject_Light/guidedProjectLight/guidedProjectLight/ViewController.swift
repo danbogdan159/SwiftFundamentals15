d06e924add0b5acc72b744ac980ab799ba1e0dae
danbogdan159 <danbogdan159@gmail.com>
2025-02-07 13:44:59 -0500

    Completed Project 1

---
+++ Unit1/GuidedProject_Light/guidedProjectLight/guidedProjectLight/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  guidedProjectLight
+//
+//  Created by spring2025 on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        // Do any additional setup after loading the view.
+    }
+
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+        //if lightOn {
+           // view.backgroundColor = .white
+           // lightButton.setTitle("Off", for: .normal)
+        //} else {
+            //view.backgroundColor = .black
+            //lightButton.setTitle("On", for: .normal)
+        //}
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
+}
+
---
+++ Unit1/GuidedProject_Light/guidedProjectLight/guidedProjectLightTests/guidedProjectLightTests.swift
@@ -0,0 +1,17 @@
+//
+//  guidedProjectLightTests.swift
+//  guidedProjectLightTests
+//
+//  Created by spring2025 on 2/7/25.
+//
+
+import Testing
+@testable import guidedProjectLight
+
+struct guidedProjectLightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
